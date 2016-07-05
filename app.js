b57edf8a4dde6993a7821987c59f380959f6f2e6
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-07-04 17:24:23 -0700

    required npm modules

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 'use strict';
 
+require('dotenv').load();
+
 var express = require('express');
-var app = express();
-var router = express.Router();
+var path = require('path');
+var bcrypt = require('bcrypt');
+var bodyParser = require('body-parser');
+var cookieSession = require('cookie-session');
+var logger = require('morgan');
+var pg = require('pg');
 var port = 3000;
 
-router.get('/', function(req, res) {
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [process.env['KEY']]
+}));
+
+app.get('/', function(req, res) {
   console.log('Hello world!');
   res.send('hello, world');
 });
 
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('listening on port' + port);
+  console.log("Listening on port " + port);
 });
+
+module.exports = app;
