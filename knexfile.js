fb71aa88949b9f38cad047fac12fb5fe4d75315c
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-07-16 14:10:05 -0700

    migrations config

--- knexfile.js
+++ knexfile.js
@@ -9,14 +9,17 @@ module.exports = {
       min: 1,
       max: 1
     }
-  }
+  },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'postgres://pdzkzaiywoqxxe:278QDcG2LwKBGjkmqyKS3M5QH1@ec2-107-20-166-28.compute-1.amazonaws.com:5432/dbj99m1bl1kpei',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  // }
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://pdzkzaiywoqxxe:278QDcG2LwKBGjkmqyKS3M5QH1@ec2-107-20-166-28.compute-1.amazonaws.com:5432/dbj99m1bl1kpei',
+      user:     'username',
+      password: 'password'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+  }
 };
